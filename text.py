b89f2bd47d8d7091f612fd84898cadd7d9c5d51e
suryasyn <147476904+suryasyn@users.noreply.github.com>
2023-12-20 18:35:53 +0530

    Update text.py

--- text.py
+++ text.py
@@ -31,6 +31,6 @@ with container:
 if st.session_state['generated']:
     with response_container:
         for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['history'][i], is_user=True, key=str(i) + '_user')
             #message(st.session_state["history"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state["generated"][i-1], key=str(i))
